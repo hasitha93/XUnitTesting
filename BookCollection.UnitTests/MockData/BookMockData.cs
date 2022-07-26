c3670f4a629f75a53bdc6de1bf25e8200cc582f3
Hasitha Balasinghe <hasitha@jrcsoftware.com>
2022-07-26 12:47:30 +0530

    Added mock data

---
+++ BookCollection.UnitTests/MockData/BookMockData.cs
@@ -0,0 +1,52 @@
+﻿using BookCollection.API.Models;
+using BookCollection.DataAccess.Entities;
+
+namespace BookCollection.UnitTests.MockData
+{
+    public class BookMockData
+    {
+        public static List<Book> GetBookList()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    Isbn13 = 1234567890111,
+                    Title = "Book 1",
+                    Author = "Author 1",
+                    PublishedYear = 2001,
+                    CreatedDateTime = DateTime.UtcNow,
+                },
+                new Book
+                {
+                    Isbn13 = 1234567890222,
+                    Title = "Book 2",
+                    Author = "Author 2",
+                    PublishedYear = 2002,
+                    CreatedDateTime = DateTime.UtcNow,
+                },
+                new Book
+                {
+                    Isbn13 = 1234567890333,
+                    Title = "Book 3",
+                    Author = "Author 3",
+                    PublishedYear = 2003,
+                    CreatedDateTime = DateTime.UtcNow,
+                }
+            };
+        }
+
+        public static List<Book> GetEmptyBookList() => new();
+
+        public static BookModel NewBookModel()
+        {
+            return new BookModel
+            {
+                Isbn13 = 1234567890444,
+                Title = "Book 4",
+                Author = "Author 4",
+                PublishedYear = 2004
+            };
+        }
+    }
+}
